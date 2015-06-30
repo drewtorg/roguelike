f9af176e532eb2d089d9a563e2a2f3cfb3646bfe
drewtorg <drew.torgeson@gmail.com>
2015-06-29 23:02:46 -0600

    Added initial files
    Can draw basic symbols on the screen and initial implementations of keyboard movement has been implemented.

---
+++ main.py
@@ -0,0 +1,51 @@
+import libtcodpy as libtcod
+from object import Object
+
+SCREEN_WIDTH = 80;
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 20
+
+libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'Libtcod Tutorial', False)
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+player = Object(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', libtcod.white, con)
+npc = Object(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', libtcod.yellow, con)
+objects = [npc, player]
+
+def handle_keys():
+	global playerx, playery
+
+	key = libtcod.console_wait_for_keypress(True)
+
+	if key.vk == libtcod.KEY_ESCAPE:
+		return True
+
+	if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+		player.move(0, -1)
+
+	elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+		player.move(0, 1)
+
+	elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+		player.move(-1, 0)
+
+	elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+		player.move(1, 0)
+
+while not libtcod.console_is_window_closed():
+	libtcod.console_set_default_foreground(0, libtcod.white)
+
+	for obj in objects:
+		obj.draw()
+
+	libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
+	libtcod.console_flush()
+
+	for obj in objects:
+		obj.clear()
+
+	exit = handle_keys()
+	if exit:
+		break
